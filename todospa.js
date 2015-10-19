194b28c4d1c4a44e28a1054e34f225ba48c0cbb0
Noah Manternach <mantno01@luther.edu>
2015-10-18 21:23:45 -0500

    Update todospa.js

--- todospa.js
+++ todospa.js
@@ -1,7 +1,7 @@
 <script>
 add_task = function() {
-  var main
-  var list
+  var main;
+  var list;
   main=document.querySelector("main");
   list=document.querySelector("ul");
   item=document.createElement("li");
