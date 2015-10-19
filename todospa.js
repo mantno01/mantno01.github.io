024fd20f36ef6be886ae2e686bd859e1d351e0fc
Noah Manternach <mantno01@luther.edu>
2015-10-18 21:18:28 -0500

    Update todospa.js

--- todospa.js
+++ todospa.js
@@ -1,12 +1,12 @@
 add_task = function() {
   var main
   var list
-  main=document.querySelector("main")
-  list=document.querySelector("ul")
-  item=document.createElement("li")
-  ul.appendChild(item)
-  test=document.createTextNode(this is a test)
-  item.appendChild(test)
+  main=document.querySelector("main");
+  list=document.querySelector("ul");
+  item=document.createElement("li");
+  ul.appendChild(item);
+  test=document.createTextNode("this is a test");
+  item.appendChild(test);
   
   
 
