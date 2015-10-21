ca6427d55dff90e6309c2847a7423972ea7f05ae
Noah Manternach <mantno01@luther.edu>
2015-10-21 13:11:13 -0500

    Update todospa.js

--- todospa.js
+++ todospa.js
@@ -1,15 +1,31 @@
-
 add_task = function() {
   var main;
   var list;
+  var myInput;
+  var text;
+  var myPriority;
+  var check;
   main=document.querySelector("#myMain");
   list=document.querySelector("#myList");
+  myInput=document.querySelector("#inputTask");
+  myPriority=document.querySelector("#priority")
   item=document.createElement("li");
-  test=document.createTextNode("this is a test");
+  item.classList.add(myPriority.value);
+  text=document.createTextNode(myInput.value);
+  check=document.createElement("input");
+  check.type="checkbox";
+  check.onclick="done_Task";
+  list.type="none";
   list.appendChild(item);
-  item.appendChild(test);
-  
+  item.appendChild(check);
+  item.appendChild(text);
+
+           }
+
+done_Task = fucntion(){
+  if 
+}
   
 
-}
+
 
